07727ba457edcd0a6c6a5f889958719621e38803
blackeye75 <priyanshuraj22275@gmail.com>
2026-06-10 02:03:50 +0530

    feat(auth): add authentication middleware to handle JWT verification and user role management

--- services/auth/src/controller/auth.ts
+++ services/auth/src/controller/auth.ts
@@ -1,19 +1,34 @@
-import { Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import User from "../model/User.js";
 import TryCatch from "../middlewares/trycatch.js";
+import { AuthenticatedRequest } from "../middlewares/isAuth.js";
 
-export const loginUser = TryCatch(async(req,res)=>{
-   const { email, name, picture } = req.body;
-  // if (!email || !name || !picture) {
-  //  return res.status(400).json({ message: "Email, name and picture are required" });
-  // }
-  let user = await User.findOne({ email });
-  if (!user) {
-   user = await User.create({ email, name, image: picture });
+export const loginUser = TryCatch(async (req, res) => {
+ const { email, name, picture } = req.body;
+ // if (!email || !name || !picture) {
+ //  return res.status(400).json({ message: "Email, name and picture are required" });
+ // }
+ let user = await User.findOne({ email });
+ if (!user) {
+  user = await User.create({ email, name, image: picture });
+ }
+ const token = jwt.sign({ user }, process.env.JWT_SEC as string, {
+  expiresIn: "15d",
+ });
+ res.status(200).json({ message: "Logged in successfully", token, user });
+});
+
+
+const allowedRoles = ["customer","rider","seller"] as const;
+
+type Role = (typeof allowedRoles)[number];
+
+export const addUserRole = TryCatch(async(req:AuthenticatedRequest,res)=>{
+  if(!req.user?._id){
+    return res.status(401).json({message:"Unauthorized"})
   }
-  const token = jwt.sign({ user }, process.env.JWT_SEC as string, {
-   expiresIn: "15d",
-  });
-  res.status(200).json({ message: "Logged in successfully", token, user });
-} )
+  const {role} = req.body as {role:Role};
+  if( !allowedRoles.includes(role)){
+    return res.status(400).json({message:"Invalid role"})
+  }
+})
